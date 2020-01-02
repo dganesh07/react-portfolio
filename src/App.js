565206e7af0397c5e49168449f38871d2a7395b9
Damini Ganesh <daminiganesh@Daminis-MacBook-Pro.local>
2020-01-01 20:46:50 -0500

    Installed sass and added landingpage image

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import "./templates/styles.scss";
 
+import LandingImage from "./templates/black.jpg"
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-          The beginning
-      </header>
+      <div className="hello">
+        <h1>This is red car</h1>
+        <h1>This is red car</h1>
+        <h1>This is red car</h1>
+        <h1>This is red car</h1>
+        <h1>This is red car</h1>
+      </div>
     </div>
   );
 }
