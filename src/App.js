11fe7fb31668ce5f9dee233d14ee3fd87283aa59
Damini Ganesh <damini.ganesh@gmail.com>
2020-01-05 18:55:55 -0500

    Basic react router setup

--- src/App.js
+++ src/App.js
@@ -1,19 +1,44 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import "./templates/styles.scss";
 
-import LandingImage from "./templates/black.jpg"
 function App() {
   return (
-    <div className="App">
-      <div className="hello">
-        <h1>This is red car</h1>
-        <h1>This is red car</h1>
-        <h1>This is red car</h1>
-        <h1>This is red car</h1>
-        <h1>This is red car</h1>
-      </div>
-    </div>
-  );
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/about">About</Link>
+              </li>
+              <li>
+                <Link to="/users">Users</Link>
+              </li>
+            </ul>
+          </nav>
+
+          <Switch>
+            <Route path="/about">
+              <p>hello2</p>
+            </Route>
+            <Route path="/users">
+              <p>hello1</p>
+            </Route>
+            <Route path="/">
+              <p>hello</p>
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    );
 }
 
 export default App;
