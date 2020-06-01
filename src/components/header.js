4cdd87e0af1333ed6273bb53a7b2616e830c4299
Damini Ganesh <damini.ganesh@gmail.com>
2020-06-01 14:40:11 -0400

    logo will be the homepage

--- src/components/header.js
+++ src/components/header.js
@@ -9,41 +9,44 @@ import {
 import Timeline from './Timeline';
 
 function Header() {
-  var liStyle = {display: "inline-block", marginRight: "30px", fontSize: "18px"}
+  var liStyle = { display: "inline-block", marginRight: "30px", fontSize: "18px" }
   return (
     <>
-    <Router>
-      <div>
-      <p style={{display: "inline-block", marginRight: "10px", color: "#FFC68A", fontSize: "30px"}}>Damini Ganesh </p>
-        <nav style={{float: "right"}}>
-          <ul>
-            <li style={liStyle}>
-              <Link style={{ textDecoration: 'none', color: "#FFC68A"}} to="/">About</Link>
-            </li>
-            <li style={liStyle}>
-              <Link style={{ textDecoration: 'none', color: "#FFC68A"}} to="/timeline">Cove tool timeline</Link>
-            </li>
-            <li style={liStyle}>
-              <Link style={{ textDecoration: 'none' , color: "#FFC68A"}} to="/users">Contact Me</Link>
-            </li>
-          </ul>
-        </nav>
+      <Router>
+        <div>
+          <p style={{ display: "inline-block", marginRight: "10px", color: "#FFC68A", fontSize: "30px" }}><Link style={{ textDecoration: 'none', color: "#FFC68A" }} to="/">Damini Ganesh</Link> </p>
+          <nav style={{ float: "right" }}>
+            <ul>
+              <li style={liStyle}>
+                <Link style={{ textDecoration: 'none', color: "#FFC68A" }} to="/about">About</Link>
+              </li>
+              <li style={liStyle}>
+                <Link style={{ textDecoration: 'none', color: "#FFC68A" }} to="/timeline">Cove tool timeline</Link>
+              </li>
+              <li style={liStyle}>
+                <Link style={{ textDecoration: 'none', color: "#FFC68A" }} to="/resume">Resume</Link>
+              </li>
+              <li style={liStyle}>
+                <Link style={{ textDecoration: 'none', color: "#FFC68A" }} to="/contact">Contact Me</Link>
+              </li>
+            </ul>
+          </nav>
 
-        <Switch>
-          <Route path="/about">
-            <p>hello2</p>
-          </Route>
-          <Route path="/timeline">
-          <Timeline />
-          </Route>
-          <Route path="/">
-            <p>hello</p>
-          </Route>
-        </Switch>
-      </div>
-    </Router>
+          <Switch>
+            <Route path="/about">
+              <p>hello2</p>
+            </Route>
+            <Route path="/timeline">
+              <Timeline />
+            </Route>
+            <Route path="/">
+              <p>hello</p>
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     </>
-    );
+  );
 }
 
 export default Header;
