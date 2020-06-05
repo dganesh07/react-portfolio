35082d1304a4695b9c90c47be06f77abfedf427c
Damini Ganesh <damini.ganesh@gmail.com>
2020-06-05 00:58:16 -0400

    hero of the website and starting styled components

--- src/components/header.js
+++ src/components/header.js
@@ -1,46 +1,77 @@
-import React from 'react';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
-import Timeline from './Timeline';
+import Timeline from "./Timeline";
+import Hero from "./Hero";
 
 function Header() {
-  var liStyle = { display: "inline-block", marginRight: "30px", fontSize: "18px" }
+  var liStyle = {
+    display: "inline-block",
+    marginRight: "30px",
+    fontSize: "18px",
+  };
   return (
     <>
       <Router>
         <div>
-          <p style={{ display: "inline-block", marginRight: "10px", color: "#FFC68A", fontSize: "30px" }}><Link style={{ textDecoration: 'none', color: "#FFC68A" }} to="/">Damini Ganesh</Link> </p>
+          <p
+            style={{
+              display: "inline-block",
+              marginRight: "10px",
+              color: "#FFC68A",
+              fontSize: "30px",
+            }}
+          >
+            <Link style={{ textDecoration: "none", color: "#FFC68A" }} to="/">
+              Damini Ganesh
+            </Link>{" "}
+          </p>
           <nav style={{ float: "right" }}>
             <ul>
               <li style={liStyle}>
-                <Link style={{ textDecoration: 'none', color: "#FFC68A" }} to="/about">About</Link>
+                <Link
+                  style={{ textDecoration: "none", color: "#FFC68A" }}
+                  to="/about"
+                >
+                  About
+                </Link>
               </li>
               <li style={liStyle}>
-                <Link style={{ textDecoration: 'none', color: "#FFC68A" }} to="/timeline">Cove tool timeline</Link>
+                <Link
+                  style={{ textDecoration: "none", color: "#FFC68A" }}
+                  to="/timeline"
+                >
+                  Cove tool timeline
+                </Link>
               </li>
               <li style={liStyle}>
-                <Link style={{ textDecoration: 'none', color: "#FFC68A" }} to="/resume">Resume</Link>
+                <Link
+                  style={{ textDecoration: "none", color: "#FFC68A" }}
+                  to="/resume"
+                >
+                  Resume
+                </Link>
               </li>
               <li style={liStyle}>
-                <Link style={{ textDecoration: 'none', color: "#FFC68A" }} to="/contact">Contact Me</Link>
+                <Link
+                  style={{ textDecoration: "none", color: "#FFC68A" }}
+                  to="/contact"
+                >
+                  Contact Me
+                </Link>
               </li>
             </ul>
           </nav>
 
           <Switch>
             <Route path="/about">
-              <p>hello2</p>
+              <p>hello</p>
             </Route>
             <Route path="/timeline">
               <Timeline />
             </Route>
             <Route path="/">
-              <p>hello</p>
+              <Hero />
             </Route>
           </Switch>
         </div>
