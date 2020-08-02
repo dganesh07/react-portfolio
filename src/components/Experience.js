e6b8e46e448f1c282d6c64e7cc970ee4e1c20105
Damini Ganesh <damini.ganesh@gmail.com>
2020-08-01 20:22:36 -0400

    trying out react spring for some animation

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -3,18 +3,34 @@ import TabComponent from "./tabs";
 import styled from "styled-components";
 import img from "../java.png";
 
+// import { useSpring, animated, config } from "react-spring";
+import { Spring } from "react-spring/renderprops";
+import VisibilitySensor from "react-visibility-sensor";
+
 function Experience() {
+  // const props = useSpring({
+  //   delay: 500,
+  //   opacity: isVisible ? 1 : 0,
+  // });
+
   const ExperienceWrapper = styled.section`
     border: 1px solid white;
-    background-image: url(${img});
-    height: 660px;
+    height: 670px;
   `;
   return (
     <ExperienceWrapper>
-      <div className="row">
-        <div className="col-md-8">
-          <TabComponent />
-        </div>
+      <div className="">
+        <VisibilitySensor once>
+          {({ isVisible }) => (
+            <Spring delay={200} to={{ opacity: isVisible ? 1 : 0 }}>
+              {({ opacity }) => (
+                <div style={{ opacity }}>
+                  <TabComponent />
+                </div>
+              )}
+            </Spring>
+          )}
+        </VisibilitySensor>
       </div>
     </ExperienceWrapper>
   );
